83b42545f9340a4430a4aadc1d2ff324f04cf649
Vazgen <76519446+vazgenM2@users.noreply.github.com>
2022-02-20 10:02:05 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ window.onload = function () {
 
 	// ====================================== Generating blocks
 	let blockDir = true
+	let destroyedBlock;
 	let blockX = 0
 	let blockY = 0
 	let figureParams = [0, 0]
@@ -37,6 +38,8 @@ window.onload = function () {
 		if (block.classList.contains('black')) {
 			if (i >= 0 && i <= 23) generateFigure(block, 'white')
 			if (i >= 39 && i <= 63) generateFigure(block, 'black')
+			if (i >= 0 && i <= 7) block.classList.add('special-black')
+			if (i >= 55 && i <= 63) block.classList.add('special-white')
 		}
 
 		block.setAttribute('x', blockX)
@@ -52,27 +55,69 @@ window.onload = function () {
 				figureParams = [+block.getAttribute('x'), +block.getAttribute('y')]
 				for (let i = 0; i < document.querySelectorAll('.block').length; i++) {
 					document.querySelectorAll('.block')[i].classList.remove('select')
+					document.querySelectorAll('.block')[i].classList.remove('destroy')
 				}
 				if (block.children[0].classList[1] == 'black') {
-					if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`)
-						&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).children.length) {
-						document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).classList.add('select')
+					try {
+						if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`)
+							&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).children.length) {
+							document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).classList.add('select')
+						}
+						else if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).children?.length
+							&& document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`) &&
+							!document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`).children.length
+							&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).children[0].classList.contains('black')) {
+							document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`).classList.add('select')
+							document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`).classList.add('destroy')
+							destroyedBlock = document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`)
+						}
+
+					} catch (e) {
+						console.log(e)
 					}
-					else if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') - 1}`).children.length
-						&& document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`) &&
-						!document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`).children.length) {
-						document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') - 2}`).classList.add('select')
+					try {
+						if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`)
+							&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).children.length) {
+							document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).classList.add('select')
+						}
+						else if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).children.length
+							&& document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`) &&
+							!document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`).children.length
+							&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).children[0].classList.contains('black')) {
+							document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`).classList.add('select')
+							document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`).classList.add('destroy')
+							destroyedBlock = document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`)
+						}
+					} catch (e) {
+						console.log(e)
 					}
-					if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`)
-						&& !document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).children.length) {
-						document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).classList.add('select')
+					try {
+						if (document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') + 1}`).children.length
+							&& document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') + 2}`) &&
+							!document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') + 2}`).children.length
+							&& !document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') + 1}`).children[0].classList.contains('black')) {
+							document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') + 2}`).classList.add('destroy')
+							document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') + 2}`).classList.add('select')
+							destroyedBlock = document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') + 1}`)
+						}
+					} catch (e) {
+						throw e
 					}
-					else if (document.querySelector(`.y${+block.getAttribute('y') - 1}.x${+block.getAttribute('x') + 1}`).children.length
-						&& document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`) &&
-						!document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`).children.length) {
-						document.querySelector(`.y${+block.getAttribute('y') - 2}.x${+block.getAttribute('x') + 2}`).classList.add('select')
+					try {
+						if (document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') - 1}`).children.length
+							&& document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') - 2}`) &&
+							!document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') - 2}`).children.length
+							&& !document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') - 1}`).children[0].classList.contains('black')) {
+							document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') - 2}`).classList.add('destroy')
+							document.querySelector(`.y${+block.getAttribute('y') + 2}.x${+block.getAttribute('x') - 2}`).classList.add('select')
+							destroyedBlock = document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') - 1}`)
+						}
+					} catch (e) {
+						throw e
 					}
+
 				}
+				// ======================================== White figures
 				else {
 					if (document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') - 1}`)
 						&& !document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') - 1}`).children.length) {
@@ -83,9 +128,14 @@ window.onload = function () {
 						document.querySelector(`.y${+block.getAttribute('y') + 1}.x${+block.getAttribute('x') + 1}`).classList.add('select')
 					}
 				}
+				// ========================================== Click on the block
 			} else if (block.classList.contains('select')) {
+				if (block.classList.contains('destroy')) {
+					destroyedBlock.innerHTML = ''
+				}
 				for (let i = 0; i < document.querySelectorAll('.block').length; i++) {
 					document.querySelectorAll('.block')[i].classList.remove('select')
+					document.querySelectorAll('.block')[i].classList.remove('destroy')
 				}
 				block.appendChild(document.querySelector(`.y${figureParams[1]}.x${figureParams[0]}`).children[0])
 			}
